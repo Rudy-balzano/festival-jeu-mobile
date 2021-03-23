962013e2edcd170fb8bbd0b0169a50685b4806ea
Balzano <rudybalzano16@gmail.com>
2021-03-23 11:37:36 +0100

    Initial Commit

---
+++ festival-jeu-mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  festival-jeu-mobile
+//
+//  Created by Rudy Balzano on 23/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ festival-jeu-mobile/festival_jeu_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  festival_jeu_mobileApp.swift
+//  festival-jeu-mobile
+//
+//  Created by Rudy Balzano on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct festival_jeu_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
